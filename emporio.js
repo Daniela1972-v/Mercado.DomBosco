6600b3d734f2394ca4bb7b72e7da1f9347ee1b18
Daniela Varela <dannyvsouza@hotmail.com>
2024-12-06 12:25:49 -0300

    Mercado Dom Bosco
    Finalizando a Pagina

--- emporio.js
+++ emporio.js
@@ -1,4 +1,27 @@
-  //Aqui adicionamos um evento de escutar o click do Mouse sobre o ícone de Hamburguer.
+window.revelar = ScrollReveal({reset:true});
+
+
+revelar.reveal('.efeito-img-topo', {
+    duration: 2000,
+    distance: '90px'
+})
+
+
+revelar.reveal('.clientes-efeito', {
+    duration: 2000,
+    distance: '120px'
+})
+
+revelar.reveal('.principais-produtos', {
+    duration: 2000,
+    distance: '200px'
+})
+ 
+ 
+ 
+
+ 
+ //Aqui adicionamos um evento de escutar o click do Mouse sobre o ícone de Hamburguer.
  hamburguerButton.addEventListener("click", function() {
     mobileMenu.classList.add("flex");
     //Criamos uma função que ouvir o click do Mouse, ele adicione uma Class, que está fazendo nossa caixa (Div) aparecer, ou seja, ficar visível
